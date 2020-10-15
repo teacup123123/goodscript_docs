7c75fb453c9c7458b719acda2e0ce1e8b955cfea
qnano <qnano.biu.ac.il@gmail.com>
2020-10-15 18:58:13 +0300

    change style

--- source/conf.py
+++ source/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
+
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(r'C:\Users\qnano\Documents\pyviewlabunapera\goodScripts'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -22,13 +22,15 @@ project = 'goodScripts'
 copyright = '2020, Tikai'
 author = 'Tikai'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,13 +40,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap-astropy'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
